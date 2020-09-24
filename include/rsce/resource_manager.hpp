d81476960bea3cc3652ec75185c832223a230d56
Aymeric Pellé <aympelle@gmail.com>
2020-09-24 16:35:50 +0200

    v0.1.0:RSCE#5: Add class resource_manager.

---
+++ include/rsce/resource_manager.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "basic_resource_manager.hpp"
+#include <vlfs/vlfs.hpp>
+
+namespace rsce
+{
+class resource_manager : public basic_resource_manager
+{
+public:
+    explicit resource_manager(vlfs::virtual_filesystem& vlfs) : vlfs_(&vlfs) {}
+
+    inline const vlfs::virtual_filesystem* virtual_filesystem() const { return vlfs_; }
+    inline vlfs::virtual_filesystem* virtual_filesystem() { return vlfs_; }
+
+    template <class resource>
+    inline std::shared_ptr<resource> get(const std::filesystem::path& rsc_path)
+    {
+        return this->basic_resource_manager::get<resource>(vlfs_->real_path(rsc_path));
+    }
+
+    template <class resource>
+    inline resource& get_ref(const std::filesystem::path& rsc_path)
+    {
+        return this->basic_resource_manager::get_ref<resource>(vlfs_->real_path(rsc_path));
+    }
+
+    template <class resource>
+    inline bool insert(const std::filesystem::path& rsc_path, std::shared_ptr<resource> rsc_sptr)
+    {
+        return this->basic_resource_manager::insert<resource>(vlfs_->real_path(rsc_path), rsc_sptr);
+    }
+
+    template <class resource>
+    inline void set(const std::filesystem::path& rsc_path, std::shared_ptr<resource>&& rsc_sptr)
+    {
+        this->basic_resource_manager::set<resource>(vlfs_->real_path(rsc_path), std::move(rsc_sptr));
+    }
+
+    template <class resource>
+    inline void set(const std::filesystem::path& rsc_path, resource&& rsc)
+    {
+        this->basic_resource_manager::set<resource>(vlfs_->real_path(rsc_path), std::move(rsc));
+    }
+
+    template <class resource>
+    inline std::shared_ptr<resource> load(const std::filesystem::path& rsc_path)
+    {
+        return this->basic_resource_manager::load<resource>(vlfs_->real_path(rsc_path));
+    }
+
+    template <class resource>
+    inline void remove(const std::filesystem::path& rsc_path)
+    {
+        this->basic_resource_manager::remove<resource>(vlfs_->real_path(rsc_path));
+    }
+
+private:
+    vlfs::virtual_filesystem* vlfs_ = nullptr;
+};
+}
